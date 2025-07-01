42f4b1f74f3a747cd0f432b9754346b07cbbc607
“Jessica <jp3864@columbia.edu>
2025-06-30 23:25:49 -0400

    Remove lint quotations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add this block to override specific rules
+  {
+    rules: {
+      quotes: "off",
+      "react/no-unescaped-entities": "off"
+    },
+  },
 ];
 
 export default eslintConfig;
